d77219bc6c6f5e005adc6c55ff92f4b195801b14
name1e5s <name1e5s@qq.com>
2019-06-13 00:16:34 +0800

    WB: add write back beta

---
+++ writeback_beta.sv
@@ -0,0 +1,21 @@
+`timescale 1ns / 1ps
+`include "common.vh"
+module writeback_beta(
+        input                       clk,
+        input                       rst,
+        input [31:0]                result,
+        input [4:0]                 reg_dest,
+        input                       write_en,
+
+        output logic                reg_write_en,
+        output logic [4:0]          reg_write_dest,
+        output logic [31:0]         reg_write_data
+);
+
+    always_comb begin : generate_output
+        reg_write_en = write_en;
+        reg_write_dest = reg_dest;
+        reg_write_data = result;
+    end
+
+endmodule
