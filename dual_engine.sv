f0da4a1b6b48a3a74712e96d3ec3945f5978522e
name1e5s <name1e5s@qq.com>
2019-06-15 20:25:44 +0800

    Preparation for dual issue engine

---
+++ dual_engine.sv
@@ -0,0 +1,12 @@
+`timescale 1ns / 1ps
+// Dual issue detect engine.
+module dual_engine(
+        input                           alpha_issue,
+        input [31:0]                    instruction_alpha,
+        input [31:0]                    instruction_beta,
+        output logic                    beta_issue
+);
+
+// TODO：
+
+endmodule
