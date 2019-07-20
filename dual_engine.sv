98b1089a77c6b9c6a80383fee87436a936995c93
name1e5s <name1e5s@qq.com>
2019-07-20 15:28:40 +0800

    DUAL ENGINE: better code style

--- dual_engine.sv
+++ dual_engine.sv
@@ -32,25 +32,32 @@ module dual_engine(
 
     wire fifo = ~(fifo_empty || fifo_almost_empty);
     logic load_use_stall_slave;
+    logic _enable_slave;
+    assign enable_slave = _enable_slave && (!load_use_stall_slave) && fifo;
+
     always_comb begin : check_slave_enable
-        if((!enable_master) || (id_priv_inst_master) || 
-            (id_priv_inst_slave) || (|id_mem_type_slave) ||
-            id_is_branch_instr_slave || id_is_hilo_accessed_master || id_is_hilo_accessed_slave)
-            enable_slave = 1'b0;
+        if( (!enable_master) ||
+            (id_priv_inst_master) ||
+            (id_priv_inst_slave) ||
+            (id_is_branch_instr_slave) ||
+            (id_mem_type_slave != `MEM_NOOP) ||
+            (id_is_hilo_accessed_slave)
+        ) begin
+            _enable_slave = 1'b0;
+        end
         else begin
-            if(id_wb_reg_en_master) begin
-                if(id_opcode_slave == 5'd0) begin
-                    enable_slave = (~(|id_wb_reg_dest_master)) && (~((id_wb_reg_dest_master == id_rs_slave) || 
-                                      (id_wb_reg_dest_master == id_rt_slave))) && fifo && (~load_use_stall_slave);
+            if(id_wb_reg_en_master && (id_wb_reg_dest_master != 5'd0)) begin
+                if(id_opcode_slave == 6'd0) begin // R type
+                    _enable_slave = (|((id_wb_reg_dest_master ^ id_rs_slave) & (id_wb_reg_dest_master ^ id_rt_slave)));
                 end
                 else begin
-                    enable_slave =  (~(|id_wb_reg_dest_master)) && (~(id_wb_reg_dest_master == id_rs_slave)) && fifo && (~load_use_stall_slave);
-                end 
+                    _enable_slave = (|((id_wb_reg_dest_master ^ id_rs_slave)));
+                end
             end
             else begin
-                enable_slave = fifo && (~load_use_stall_slave);
+                _enable_slave = 1'b1;
             end
-        end 
+        end
     end
 
     always_comb begin
