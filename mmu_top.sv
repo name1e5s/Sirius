b0cc3577f572d667654e17559dcf553ff817172d
name1e5s <name1e5s@qq.com>
2019-08-13 00:01:47 +0800

    nscscc: fix

--- mmu_top.sv
+++ mmu_top.sv
@@ -93,7 +93,7 @@ module mmu_top(
 
     always_comb begin
         iaddr_psy   = inst_addr;
-        iaddr_type  = 1'd0;
+        iaddr_type  = inst_uncached;
         daddr_psy   = data_addr;
         daddr_type  = data_uncached;
     end
